d2925842752755409d7d60c5f2a7557c1119edce
akshaykumarvikram <akshaykumarvikram@yahoo.com>
2017-06-04 12:38:32 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 //require('./connectorSetup.js')();
 require('dotenv-extended').load();
-var request = require("sync-request")
+var sync_request = require("sync-request")
 var cheerio = require("cheerio")
 var builder = require('botbuilder'),
     fs = require('fs'),
     needle = require('needle'),
     restify = require('restify'),
-    //request = require('request'),
+    request = require('request'),
     url = require('url'),
     speechService = require('./speech-service.js');
+var request_promise = require('request-promise').defaults({ encoding: null });
+var Promise = require('bluebird');
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
@@ -30,6 +32,13 @@ var intents = new builder.IntentDialog({recognizers: [recognizer]});
 
 intents.matches('pico','/pico');
 intents.matches('getdatafromemr','/EMR');
+intents.matches('isAlergic','/isAlergic');
+intents.matches('caseReport','case_report')
+intents.matches('changeintervention','/change_intervention');
+intents.matches('changecontrol','/change_control');
+intents.matches('current_medication','/current_medication');
+intents.matches('surgery','/surgery');
+intents.onDefault('/confused');
 
 
 //Bot listening for inbound backchannel events - in this case it only listens for events named "buttonClicked"
@@ -42,6 +51,74 @@ bot.on("event", function (event) {
     bot.send(msg);
 })
 bot.dialog('/',intents);
+
+bot.dialog('/change_intervention',[
+    function(session,args,next){
+        session.userData.new_i = builder.EntityRecognizer.findEntity(args.entities,"new_intervention");
+        session.userData.i = session.userData.new_i;
+        session.userData.search_term = '(('+session.userData.p+') AND ('+session.userData.i+') AND ('+session.userData.c+') AND ('+session.userData.o+'))'
+      session.userData.pubmedlink = 'https://www.ncbi.nlm.nih.gov/pubmed?term='+encodeURIComponent(session.userData.search_term);
+      //session.send(session.userData.pubmedlink)
+     var reply = createEvent("changeURL", session.userData.pubmedlink, session.message.address);
+        session.endDialog(reply);
+
+    }
+]);
+
+bot.dialog('/change_control',[
+    function(session,args,next){
+        session.userData.new_i = builder.EntityRecognizer.findEntity(args.entities,"new_control");
+        session.userData.i = session.userData.new_i;
+        session.userData.search_term = '(('+session.userData.p+') AND ('+session.userData.i+') AND ('+session.userData.c+') AND ('+session.userData.o+'))'
+      session.userData.pubmedlink = 'https://www.ncbi.nlm.nih.gov/pubmed?term='+encodeURIComponent(session.userData.search_term);
+      //session.send(session.userData.pubmedlink)
+     var reply = createEvent("changeURL", session.userData.pubmedlink, session.message.address);
+        session.endDialog(reply);
+
+    }
+]);
+
+bot.dialog('/isAlergic',[
+    function(session,args,next){
+        var allergy = builder.EntityRecognizer.findEntity(args.entities,"allergy");
+        if(session.userData.patient_allergies.indexOf(allergy)> -1){
+            session.endDialog('Yes, the patient is allergic to '+allergy);
+        } else {
+            session.endDialog('No, the patient is not allergic to ')
+        }
+    }
+]);
+bot.dialog('/current_medication',[
+    function(session,args,next){
+        //var allergy = builder.EntityRecognizer.findEntity(args.entities,"allergy");
+        session.endDialog('The patient is currently on '+session.userData.patient_current_medication);
+    }
+]);
+bot.dialog('/surgery',[
+    function(session,args,next){
+        //var allergy = builder.EntityRecognizer.findEntity(args.entities,"allergy");
+        session.endDialog(session.userData.patient_history_surgery);
+    }
+]);
+
+bot.dialog('/case_report',[
+    function(session,args,next){
+        //var allergy = builder.EntityRecognizer.findEntity(args.entities,"allergy");
+        session.endDialog(session.userData.patient_present_illness);
+    }
+]);
+
+bot.dialog('/isalergic',[
+    function(session,args,next){
+        var allergy = builder.EntityRecognizer.findEntity(args.entities,"allergy");
+        if(session.userData.patient_allergies.indexOf(allergy)> -1){
+            session.endDialog('Yes, the patient is allergic to '+allergy);
+        } else {
+            session.endDialog('No, the patient is not allergic to ')
+        }
+    }
+]);
+
 
 bot.dialog('/pico',[
     function(session,args,next){
@@ -59,6 +136,12 @@ bot.dialog('/pico',[
         session.userData.c = session.userData.c.entity;
         if(session.userData.c.indexOf('john')> -1){session.userData.c = "st john's wort"}
         session.userData.o = session.userData.o.entity;
+
+      session.userData.search_term = '(('+session.userData.p+') AND ('+session.userData.i+') AND ('+session.userData.c+') AND ('+session.userData.o+'))'
+      session.userData.pubmedlink = 'https://www.ncbi.nlm.nih.gov/pubmed?term='+encodeURIComponent(session.userData.search_term);
+      //session.send(session.userData.pubmedlink)
+      var reply = createEvent("changeURL", session.userData.pubmedlink, session.message.address);
+      session.send(reply);
         
         session.userData.p_alt = find_alt_term(session.userData.p);
         session.userData.i_alt = find_alt_term(session.userData.i);
@@ -116,18 +199,91 @@ bot.dialog('/pico',[
       //session.userData.pubmedlink = https://www.ncbi.nlm.nih.gov/pubmed?term=((depressive%20disorder)%20AND%20ssri)%20AND%20(hypericum%20OR%20st%20john%27s%20wort)&cmd=correctspelling;
       session.userData.search_term = '(('+session.userData.p+') AND ('+session.userData.i+') AND ('+session.userData.c+') AND ('+session.userData.o+'))'
       session.userData.pubmedlink = 'https://www.ncbi.nlm.nih.gov/pubmed?term='+encodeURIComponent(session.userData.search_term);
-      session.send(session.userData.pubmedlink)
+      //session.send(session.userData.pubmedlink)
      var reply = createEvent("changeURL", session.userData.pubmedlink, session.message.address);
         session.endDialog(reply);
     },
+]);bot.dialog('/emr',[
+    function(session,args,next){
+        builder.Prompts.attachment(session,'Please upload the EMR');
+    },
+    function(session,results,next){
+        var msg = session.message;
+        var attachment = msg.attachments[0];
+        console.log('---------------------------Image received---------------------------')
+        if (attachment) {
+
+            // Message with attachment, proceed to download it.
+            // Skype & MS Teams attachment URLs are secured by a JwtToken, so we need to pass the token from our bot.
+            console.log(attachment);
+
+            var fileDownload = new Promise(
+                function(resolve, reject) {
+                    var check = checkRequiresToken(msg);
+                    if  (check==true) {
+                        resolve(requestWithToken(attachment.contentUrl));
+                    } else {
+                        resolve(request_promise(attachment.contentUrl));
+                    }
+                }
+            );
+
+            fileDownload.then(
+                function (response) {
+
+                readImageText(response, attachment.contentType, function (error, response, body) {
+                    session.userData.EMRText = (extractText(body));
+                    session.send(session.userData.EMRText)
+                    LUIS_link = process.env.LUIS_MODEL_URL
+                    var url = LUIS_link + encodeURIComponent(session.userData.EMRText)
+                    var res = sync_request('GET', url);
+                    var EMRjson = JSON.parse(res.getBody('utf8'))
+                    console.log(EMR)
+                    
+                   
+                    
+                });
+
+                }).catch(function (err, reply) {
+                    console.log('Error with attachment: ', { 
+                        statusCode: err.statusCode, 
+                        message: err });
+                        session.send("Error with attachment or reading image with %s", err);
+            });
+        } 
+    },
+   
+    //}
 ]);
-//Basic root dialog which takes an inputted color and sends a changeBackground event. No NLP, regex, validation here - just grabs input and sends it back as an event. 
-/*bot.dialog('/', [
-    function (session) {
-        var reply = createEvent("changeBackground", session.message.text, session.message.address);
-        session.endDialog(reply);
+
+bot.dialog('/EMR',[
+    function(session,args,next){
+        builder.Prompts.attachment(session,'Please upload the EMR');
+    },
+    function(session,results,next){
+        var msg = session.message;
+        var attachment = msg.attachments[0];
+        session.userData.patient_name = 'Chandler Bing';
+        session.userData.patient_age = '48';
+        session.userData.patient_sex = 'Male';
+        session.userData.patient_present_illness = '30 Year old male patient is diagnosed with depression. He would prefer to alternative medicines rather than SSRI. He was complaining of blurred vision for the past 1 month. He finds it so difficult for him to read clearly and is even affecting his driving. He also notes that he has to be getting up to the bathroom frequently, esp. at night. He now routinely gets up to urinate 3-4 times a night. He is not aware of any particular weight loss, but does feel thirsty much of the time.';
+        session.userData.patient_history_illness = 'Hypertension';
+        session.userData.patient_history_surgery = 'No surgery till date';
+        session.userData.patient_allergies = 'pencillin';
+        session.userData.patient_current_medication = '	METOPROLOL 25MG XL TAB Qty: 90 for 90 ACTIVE days Sig: TAKE ONE TABLET MOUTH QDAY Refills: 0 FOR THE HEART';
+        session.userData.patient_history_family = 'Diabetes: Father, Sibling, Grandparent';
+        session.endDialog('Patient data obtained')
+    }]);
+
+    bot.dialog('/confused',[
+    function (session,args,next){
+        session.sendTyping();
+        session.send('Pardon me. I did\'t understand that. Please try again');
+        session.endDialog();
     }
-]);*/
+]);
+
+
  //'''''''''''''''''''''''''''''''''''''''''//
  //        helper functions                //
 //'''''''''''''''''''''''''''''''''''''''//
@@ -137,7 +293,7 @@ function find_alt_term(searchterm) {
     var url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=" + encodeURIComponent(searchterm) +"&format=json";
     
     
-    var res = request('GET', url);
+    var res = sync_request('GET', url);
     var json = JSON.parse(res.getBody('utf8'))
    // console.log(json)
     
@@ -151,6 +307,110 @@ function find_alt_term(searchterm) {
             return(splitted_terms[0])
         }
     }
+};
+// Helper methods
+
+// Request file with Authentication Header
+var requestWithToken = function (url) {
+    return obtainToken().then(function (token) {
+        return request_promise({
+            url: url,
+            headers: {
+                'Authorization': 'Bearer ' + token,
+                'Content-Type': 'application/octet-stream'
+            }
+        });
+    });
+};
+
+// Promise for obtaining JWT Token (requested once)
+var obtainToken = Promise.promisify(connector.getAccessToken.bind(connector));
+
+var checkRequiresToken = function (message) {
+    return message.source === 'skype' || message.source === 'msteams';
+};
+//=========================================================
+// Vision Service
+//=========================================================
+
+// A request with binary image data to OCR API
+var readImageText = function _readImageText(url, content_type, callback) {
+
+    var options = {
+        method: 'POST',
+        url: 'https://eastus2.api.cognitive.microsoft.com/vision/v1.0/ocr?language=unk&detectOrientation =true HTTP/1.1',
+        headers: {
+            'Ocp-Apim-Subscription-Key': '03da9800160e4929a3eb34358b82ec57',
+            'Content-Type': 'application/octet-stream'
+        },
+        body: url,
+        json: false
+    };
+    request(options, callback);
+
+};
+
+var readImageTextUrl = function _readImageTextUrl(url, content_type, callback) {
+
+    var options = {
+        method: 'POST',
+        url: config.CONFIGURATIONS.COMPUTER_VISION_SERVICE.API_URL + "ocr/",
+        headers: {
+            'ocp-apim-subscription-key': config.CONFIGURATIONS.COMPUTER_VISION_SERVICE.API_KEY,
+            'content-type': content_type
+        },
+        body: {url: url, language: "en"},
+        json: true
+    };
+
+    request(options, callback);
+
+};
+var extractText = function _extractText(bodyMessage) {
+
+    var bodyJson = bodyMessage;
+
+    // The attached images are json strings, the urls are not
+    //  so only convert if we need to
+    if (IsJsonString(bodyMessage)) {
+        bodyJson = JSON.parse(bodyMessage);
+    }
+
+    // The "regions" - part of the json to drill down first level
+    var regs = bodyJson.regions;
+    text = "";
+
+    if (typeof regs === "undefined") {return "Something's amiss, please try again.";};
+
+    // Get line arrays
+    var allLines = regs.map(x => x.lines);
+    // Flatten array
+    var allLinesFlat =  [].concat.apply([], allLines);
+    // Get the words objects
+    var allWords = allLinesFlat.map(x => x.words);
+    // Flatten array
+    var allWordsFlat = [].concat.apply([], allWords);
+    // Get the text
+    var allText = allWordsFlat.map(x => x.text);
+    // Flatten
+    var allTextFlat = [].concat.apply([], allText);
+
+    text = allTextFlat.join(" ");
+
+    if (text) {
+        return text;
+    } else {
+        return "Could not find text in this image. :( Try again?";
+    }
+};
+
+function IsJsonString(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
 }
 //Creates a backchannel event
 const createEvent = (eventName, value, address) => {
